e823aa379a6c75540bb2165dd6f49ccfd559f1f3
Chu-Hsiang Wu <shawnwu@utexas.edu>
2017-09-20 18:39:59 -0500

    update talkmap according to files in _talk.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "The Woodlands, TX, USA", 
+    30.1734194, 
+    -95.504686
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Lafayette, Louisiana, USA", 
+    30.2240897, 
+    -92.0198427
   ]
 ];
